f98349f95b5b6d09bd9541527bfe8b0d2320d0fe
Genesissongag <genesissongag@gmail.com>
2024-10-02 11:25:36 +0800

    Create evenNumbers.js

---
+++ evenNumbers.js
@@ -0,0 +1,7 @@
+// Write a program that prints numbers from 1 to 10 using a for loop
+for (let i = 1; i <= 10; i++) {
+  // Modify the program to print only even numbers between 1 and 10
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
